418d03a719be22feddf10029bcf8063f2bf2a84d
Tecticher <tecticher@gmail.com>
2015-09-28 16:40:44 +0200

    Fixed null pointer exception

--- src/ch/makery/address/view/PersonEditDialogController.java
+++ src/ch/makery/address/view/PersonEditDialogController.java
@@ -1,14 +1,17 @@
 package ch.makery.address.view;
 
-import ch.makery.address.model.Person;
-import ch.makery.address.util.DateUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
 
 /**
- * Created by usuario on 27/9/15.
+ * Dialog to edit details of a person.
+ *
+ * @author Marco Jakob
  */
 public class PersonEditDialogController {
 
@@ -25,22 +28,36 @@ public class PersonEditDialogController {
     @FXML
     private TextField birthdayField;
 
+
     private Stage dialogStage;
     private Person person;
     private boolean okClicked = false;
 
-
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
     @FXML
     private void initialize() {
-
     }
 
-    public void setDialogStage(Stage stage) {
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
     }
 
+    /**
+     * Sets the person to be edited in the dialog.
+     *
+     * @param person
+     */
     public void setPerson(Person person) {
         this.person = person;
+
         firstNameField.setText(person.getFirstName());
         lastNameField.setText(person.getLastName());
         streetField.setText(person.getStreet());
@@ -130,7 +147,7 @@ public class PersonEditDialogController {
             return true;
         } else {
             // Show the error message.
-            Alert alert = new Alert(Alert.AlertType.ERROR);
+            Alert alert = new Alert(AlertType.ERROR);
             alert.initOwner(dialogStage);
             alert.setTitle("Invalid Fields");
             alert.setHeaderText("Please correct invalid fields");
@@ -141,5 +158,4 @@ public class PersonEditDialogController {
             return false;
         }
     }
-
 }
